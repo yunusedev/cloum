30d4620601f9e0c50be4c0a453a48cfe4b114f9e
Yunus Emre <yunusemregun222@icloud.com>
2025-08-14 16:14:54 +0300

    fixed

--- components/general/home/Waitlist.tsx
+++ components/general/home/Waitlist.tsx
@@ -5,7 +5,7 @@ export default function Waitlist() {
     return(
         <section className="flex items-center px-8 flex-col gap-6">
             <h1 className="text-3xl font-bold">Bekleme listesi</h1>
-            <p className="text-muted">Bekleme listesine katılarak gelişmelerden haberdar ol</p>
+            <p className="text-muted text-center">Bekleme listesine katılarak gelişmelerden haberdar ol</p>
             <div className="flex items-center gap-2">
                 <input type="text" className="p-2.5 focus:ring-2 focus:ring-green-600 transition ring ring-tertiary rounded-lg outline-none text-[15px] font-medium placeholder:text-muted text-foreground" placeholder="E-mail adresi" />
                 <button className="p-2.5 px-5 hover:bg-accent text-[15px] bg-foreground rounded-md text-background font-semibold">Katıl</button>
